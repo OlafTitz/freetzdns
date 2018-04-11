e133a75ef81cc26d727d0820651dc4642fc3c5cd
Olaf Titz <olaf@bigred.inka.de>
2018-04-11 22:55:43 +0200

    Start microdns and update-blocklists

--- scripts/unbound.sh
+++ scripts/unbound.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 ROOT=`dirname $0`/..
+PATH=$ROOT/bin:$ROOT/sbin:$PATH
+(sleep 60; update-blocklists.sh) &
+microdns 255.255.255.255 127.0.0.1 53002 &
 export LD_LIBRARY_PATH="$ROOT/lib"
-exec "$ROOT/sbin/unbound" "$@"
+exec unbound "$@"
